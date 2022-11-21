dc1f8166fbe97ff66312968c33d2589aca21b59d
wurgo <laxvideoeffects@gmail.com>
2022-11-21 22:19:07 +0000

    fix dragon issue

--- src/main/java/me/wurgo/practiceseedmod/mixin/mechanics/entities/EnderDragonEntityMixin.java
+++ src/main/java/me/wurgo/practiceseedmod/mixin/mechanics/entities/EnderDragonEntityMixin.java
@@ -2,10 +2,7 @@ package me.wurgo.practiceseedmod.mixin.mechanics.entities;
 
 import me.wurgo.practiceseedmod.PracticeSeedMod;
 import me.wurgo.practiceseedmod.core.WorldConstants;
-import me.wurgo.practiceseedmod.core.config.ConfigWrapper;
-import me.wurgo.practiceseedmod.core.config.ConfigWriter;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.ai.pathing.PathNode;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.boss.dragon.phase.PhaseManager;
 import net.minecraft.entity.boss.dragon.phase.PhaseType;
@@ -15,7 +12,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.time.Clock;
@@ -49,33 +45,5 @@ public abstract class EnderDragonEntityMixin extends MobEntity {
         }
 
         super.tick();
-    }
-
-    @Redirect(
-            method = "getNearestPathNodeIndex()I",
-            at = @At(
-                    value = "NEW",
-                    target = "net/minecraft/entity/ai/pathing/PathNode"
-            )
-    )
-    private PathNode customPathNodeHeight1(int x, int y, int z) {
-        if (new ConfigWrapper(ConfigWriter.INSTANCE).getBoolValue("lowerNodeHeight", true)) {
-            return new PathNode(x, PracticeSeedMod.nodeHeight, z);
-        }
-        return new PathNode(x, y, z);
-    }
-
-    @Redirect(
-            method = "getNearestPathNodeIndex(DDD)I",
-            at = @At(
-                    value = "NEW",
-                    target = "net/minecraft/entity/ai/pathing/PathNode"
-            )
-    )
-    private PathNode customPathNodeHeight2(int x, int y, int z) {
-        if (new ConfigWrapper(ConfigWriter.INSTANCE).getBoolValue("lowerNodeHeight", true)) {
-            return new PathNode(x, PracticeSeedMod.nodeHeight, z);
-        }
-        return new PathNode(x, y, z);
     }
 }
